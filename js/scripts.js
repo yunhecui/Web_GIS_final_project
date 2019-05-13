40ed8cb286c495b1fc3770a5c15dc39075ec4ec7
Yunhe Cui <yc3420@nyu.edu>
2019-05-13 11:46:25 -0400

    upload link to tableau

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,10 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var divElement = document.getElementById('viz1557761578051');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
