ca27a1d75ea3556c30f3efbc40f0ce8127adb4c4
Yunhe Cui <yc3420@nyu.edu>
2019-05-13 15:56:37 -0400

    add basic map and time slider

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,32 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieXVuaGVjdWkiLCJhIjoiY2p1NDhncHp3MGJwNTQ1bnd5aTEyODdxNCJ9.8XO2bpiop4ue0tLtzDbcig';
+var map = new mapboxgl.Map({
+container: 'mapContainer',
+style: 'mapbox://styles/mapbox/light-v10',
+center: [150, 35],
+zoom: 0.55,
+});
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+Captial.forEach(function(cap) {
+
+
+  new mapboxgl.Marker({
+    color: 'purple',
+  })
+    .setLngLat([cap.long, cap.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 5 })
+    .setText(`${cap.country}, ${cap.city}`))
+    .addTo(map);
+})
+
+
+
 var divElement = document.getElementById('viz1557761578051');
 var vizElement = divElement.getElementsByTagName('object')[0];
 vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
