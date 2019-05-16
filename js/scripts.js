aa055507e6e0ad1ca7980c2c909eb4b1c94ed121
Yunhe Cui <yc3420@nyu.edu>
2019-05-16 11:35:11 -0400

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ var map = new mapboxgl.Map({
 container: 'mapContainer',
 style: 'mapbox://styles/mapbox/light-v10',
 center: [150, 35],
-zoom: 0.6,
+zoom: 0.55,
 });
 
 // disable map zoom using scroll by default
@@ -141,7 +141,7 @@ document.getElementById('resetMap').addEventListener('click', function () {
 map.scrollZoom.disable();
 map.flyTo({
   center: [150, 35],
-  zoom: 0.6,
+  zoom: 0.55,
 })
 });
 
